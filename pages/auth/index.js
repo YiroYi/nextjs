5bfe2eadb787f0020e088659efb4c2508fc3e00e
YiroYi <yiroyi@live.com>
2020-08-24 23:03:04 -0500

    NextJs Testing

---
+++ pages/auth/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import User from '../../components/User';
+const authIndexPage = () => (
+  <div>
+    <h1>The Auth index page</h1>
+    <User name="Yiro" age={33} />
+  </div>
+);
+
+export default authIndexPage;
